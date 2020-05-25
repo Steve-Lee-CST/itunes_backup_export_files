d9007a9de73b8a1b31b3faad7eb09bec5936b9c8
Steve-Lee-CST <65708028+Steve-Lee-CST@users.noreply.github.com>
2020-05-26 01:11:26 +0800

    Add step tags
    a demo with 5 steps to show the usage of the package

--- demo.py
+++ demo.py
@@ -7,15 +7,19 @@ if __name__ == "__main__":
 
     ep = exporter(manifest_db_path)
 
+    # step 1
     fd = ep.get_file_dict()
     print(f'count of file_dict: {len(fd)}')
 
+    # step 2
     fid = ep.get_file_info_dict()
     print(f'count of file_info_dict: {len(fid)}')
 
+    # step 3
     fij = ep.get_file_inner_join()
     print(f'count of file_inner_join: {len(fij)}')
 
+    # step 4
     # filter the files
     file_list = []
     for item in fij.values():
@@ -23,6 +27,7 @@ if __name__ == "__main__":
         if 'com.tencent.xin' in item['domain']:
             file_list.append(item)
     
+    # step 5
     # export files
     ep.export_files(save_path, file_list)
     print('Done') 
